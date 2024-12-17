6591777468abe0528da21a5a762d7732defa28bb
Benjamin <178317378+benjamincastor@users.noreply.github.com>
2024-12-17 09:58:07 -0500

    Update deb script (#4)
    * Update deb files
    
    * update debs
    
    * update deb
    
    * deb
    
    * gitignore
    
    ---------
    
    Co-authored-by: Benjamin Castor <bcastor@echomav.com>

--- make_debian.sh
+++ make_debian.sh
@@ -3,16 +3,10 @@
 
 set -e
 
-# Check if private_key.pem exists
-if [ ! -f private_key.pem ]; then
-  echo "Error: private_key.pem not found!"
-  exit 1
-fi
-
 PACKAGE_NAME="microhard"
 SUDO=$(test ${EUID} -ne 0 && which sudo)
 
-$SUDO apt install -y dpkg
+$SUDO apt install -y python3-paramiko dpkg
 
 cd $PACKAGE_NAME
 $SUDO cp usr/lib/python3.11/dist-packages/$PACKAGE_NAME/$PACKAGE_NAME.py usr/bin/$PACKAGE_NAME
@@ -23,13 +17,9 @@ cd ..
 DEB_FILE=$(find . -type f -name "*arm64.deb" | head -n 1)
 echo "Debian file created: $DEB_FILE"
 
-# Generate a SHA-256 Digest of the .deb File
-$SUDO openssl dgst -sha256 -out $DEB_FILE.sha256 $DEB_FILE
-# Sign the Digest with the Private Key
-$SUDO openssl dgst -sha256 -sign private_key.pem -out $DEB_FILE.sig $DEB_FILE
 echo
 echo "=======-------======="
 echo
-echo "Add $DEB_FILE.sig and $DEB_FILE to source control."
+echo "Add $DEB_FILE to source control."
 echo
 echo "=======-------======="
